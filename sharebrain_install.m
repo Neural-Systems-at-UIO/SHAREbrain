0da1d166b3d4ed7175fd23eec55df16a0cf72db7
ehennestad <ehennestad@gmail.com>
2024-11-27 15:56:31 +0100

    Update sharebrain_install.m

--- sharebrain_install.m
+++ sharebrain_install.m
@@ -3,3 +3,6 @@ addpath(genpath(fullfile(sharebrainRootFolder, 'tools')))
 
 sharebrain_installMatBox()
 matbox.installRequirements(sharebrainRootFolder)
+
+!pip install ebrains-kg-core
+!pip install fairgraph
