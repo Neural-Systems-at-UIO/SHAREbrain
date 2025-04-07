591fa97d4fde0ef667292c241ce8ca6f033368d1
ehennestad <ehennestad@gmail.com>
2025-04-07 16:09:10 +0200

    Try using pip3 on mac if pip does not work

--- sharebrain_install.m
+++ sharebrain_install.m
@@ -7,5 +7,20 @@ matbox.installRequirements(sharebrainRootFolder)
 % Run nansen_install to install all nansen's dependencies
 nansen_install()
 
-!pip install ebrains-kg-core
-!pip install fairgraph
+try
+    !pip install ebrains-kg-core
+    !pip install fairgraph
+catch ME
+    if ismac
+        try
+            !pip3 install ebrains-kg-core
+            !pip3 install fairgraph
+            clear ME
+        catch ME
+
+        end
+    end
+    if exist('ME', 'var')
+        rethrow(ME)
+    end
+end
