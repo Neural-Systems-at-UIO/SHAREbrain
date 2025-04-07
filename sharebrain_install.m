9f708c580ad2d1f6b9fe50318415c8bca866b10d
ehennestad <ehennestad@gmail.com>
2025-04-07 14:50:01 +0200

    Update sharebrain_install.m

--- sharebrain_install.m
+++ sharebrain_install.m
@@ -4,5 +4,8 @@ addpath(genpath(fullfile(sharebrainRootFolder, 'tools')))
 sharebrain_installMatBox()
 matbox.installRequirements(sharebrainRootFolder)
 
+% Run nansen_install to install all nansen's dependencies
+nansen_install()
+
 !pip install ebrains-kg-core
 !pip install fairgraph
