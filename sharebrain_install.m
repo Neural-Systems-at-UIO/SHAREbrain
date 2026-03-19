201a5284ee024ca203a714ceb011c32c51573d2b
ehennestad <ehennestad@gmail.com>
2026-03-19 10:31:10 +0100

    Remove pip installation for ebrains-kg-core and fairgraph
    Removed the installation of ebrains-kg-core and fairgraph with error handling.

--- sharebrain_install.m
+++ sharebrain_install.m
@@ -6,21 +6,3 @@ matbox.installRequirements(sharebrainRootFolder)
 
 % Run nansen_install to install all nansen's dependencies
 nansen_install()
-
-try
-    !pip install ebrains-kg-core
-    !pip install fairgraph
-catch ME
-    if ismac
-        try
-            !pip3 install ebrains-kg-core
-            !pip3 install fairgraph
-            clear ME
-        catch ME
-
-        end
-    end
-    if exist('ME', 'var')
-        rethrow(ME)
-    end
-end
