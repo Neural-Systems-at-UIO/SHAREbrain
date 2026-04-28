8bfb4bb4c3d8d7f5a9ba5382953711b9d275a5e6
ehennestad <ehennestad@gmail.com>
2026-04-28 14:36:56 +0200

    Add guarded MatNWB setup
    Make sure to use forked version of matnwb

--- sharebrain_install.m
+++ sharebrain_install.m
@@ -17,6 +17,23 @@ function sharebrain_install(flags)
         matbox.installRequirements(sharebrainRootFolder)
     end
 
+    assertValidMatNWBInstallation()
+
+    % Run matnwb's generateCore
+    generateCore()
+
     % Run nansen_install to install additional nansen dependencies
     nansen_install()
+end
+
+function assertValidMatNWBInstallation()
+    % Check if new version of MatNWB is on users path and warn
+    probeName = fullfile("+types", "+core", "TimeSeries.m");
+    s = which(probeName);
+    if ~isempty(s)
+        error("SHAREbrain:Install:IncompatibleMatNWBVersion", ...
+            ['SHAREbrain is currently relying on a forked version of ', ...
+            'MatNWB. Please remove the following version of MatNWB from you ', ...
+            'path when working with SHAREbrain: %s'], replace(s, probeName, "") )
+    end
 end
